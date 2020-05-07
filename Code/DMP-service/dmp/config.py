308ee79a075557a37bc33ce362a9b82cef2a8b4d
CatMan <garfield_lv@163.com>
2020-05-07 14:06:06 +0800

    修改配置文件，添加了测试环境下的相关配置

--- Code/DMP-service/dmp/config.py
+++ Code/DMP-service/dmp/config.py
@@ -8,35 +8,50 @@ import os
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    # SECRET_KEY
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SHTD123.'
-
+    # DB
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') or ''
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or ''
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or ''
-
 
     @staticmethod
     def init_app(app):
         pass
 #开发环境
 class DevelopmentConfig(Config):
+    # Mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or ''
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or ''
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or ''
+    # Celery
     CELERY_RESULT_BACKEND = "redis://localhost:6379/1/"
     CELERY_BROKER_URL = "redis://localhost:6379/2"
+    # DB
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'dmp-dev.sqlite')
 
 #测试环境
 class TestingConfig(Config):
-    CELERY_RESULT_BACKEND = "redis://localhost:6379/1/"
-    CELERY_BROKER_URL = "redis://localhost:6379/2"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'dmp-test.sqlite')
+    # Mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or ''
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or ''
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or ''
+    # Celery
+    CELERY_RESULT_BACKEND = "redis://192.168.3.87:6379/1"
+    CELERY_BROKER_URL = "amqp://dmp:dmp123.@192.168.3.87:5672/dmpvhost"
+    # db
+    SQLALCHEMY_DATABASE_URI = "mysql://root:shtd123.@192.168.3.87/3306/dmpdb?charset=utf-8"
+
 
 #生产环境
 class ProductionConfig(Config):
+    # Mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or ''
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or ''
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')or ''
+    # Celery
     CELERY_RESULT_BACKEND = "redis://localhost:6379/1/"
     CELERY_BROKER_URL = "redis://localhost:6379/2"
+    # DB
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'dmp.sqlite')
 
 #配置字典
@@ -44,7 +59,6 @@ config = {
     'development':DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-
     'default':DevelopmentConfig,
 }
 
