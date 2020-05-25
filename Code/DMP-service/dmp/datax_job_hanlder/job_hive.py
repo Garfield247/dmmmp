a3411ae720556caab15c1c2f1cc8b73dea913fed
CatMan <garfield_lv@163.com>
2020-05-25 15:19:29 +0800

    hive的读写

--- Code/DMP-service/dmp/datax_job_hanlder/job_hive.py
+++ Code/DMP-service/dmp/datax_job_hanlder/job_hive.py
@@ -4,10 +4,53 @@
 # @Author  : SHTD 
 
 
-def hive_reader():
-    pass
+def hive_reader(host,port,path,column,fileType,fieldDelimiter,encoding,haveKerberos,kerberosKeytabFilePath,kerberosPrincipal):
+    """
+
+    :param host:
+    :param port:
+    :param path:
+    :param column:
+    :param fileType:
+    :param fieldDelimiter:
+    :param encoding:
+    :param haveKerberos:
+    :param kerberosKeytabFilePath:
+    :param kerberosPrincipal:
+    :return:
+    """
+    hive_reader_json = {
+        "name": "hdfs_reader",
+        "parameter": {
+            "path": path,
+            "defaultFS": "hdfs://%s:%d"%(host,port),
+            "column": column if column else ["*"],
+            "fileType": fileType,
+            "encoding": encoding or "UTF-8",
+            "fieldDelimiter": fieldDelimiter or ",",
+            "csvReaderConfig": {
+                "safetySwitch": False,
+                "skipEmptyRecords": False,
+                "useTextQualifier": False
+            }
+        }
+    }
+    return hive_reader_json
 
 
 
-def hive_writer():
-    pass
+def hive_writer(host,port):
+    hive_writer_json = {
+        "name": "hdfs_writer",
+        "parameter": {
+            "defaultFS": "hdfs://%s:%d"%(host,port),
+            "fileType": "orc",
+            "path": "/user/hive/warehouse/writerorc.db/orcfull",
+            "fileName": "xxxx",
+            "column": [],
+            "writeMode": "append",
+            "fieldDelimiter": "\t",
+            "compress": "NONE"
+        }
+    }
+
---
+++ Code/DMP-service/dmp/datax_job_hanlder/job_mongodb.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2020/5/25
+# @Author  : SHTD 
+
