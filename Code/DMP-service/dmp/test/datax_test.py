3cbd28ee7256979ea430318e2853cd18caa4b72b
CatMan <garfield_lv@163.com>
2020-05-25 17:17:14 +0800

    数据同步测试脚本

--- Code/DMP-service/dmp/test/datax_test.py
+++ Code/DMP-service/dmp/test/datax_test.py
@@ -8,7 +8,7 @@ import os
 from dmp.datax_job_hanlder import *
 def test():
     r = mongodb_reader(host="192.168.3.119",port=27017,db_name="case01",collection_name="yingcai_data",username=None,password=None)
-    w = mysql_writer(host="192.168.3.87",port=3306,db="database",table="test_yc",preSql=None,postSql=None)
+    w = mysql_writer(model=1,host="192.168.3.87",port=3306,username="root",password="shtd123.",column=None,db="database",table="test_yc",preSql=None,postSql=None)
     job_jsonpath = job_hanlder(reader=r,writer=w)
 
     os.system("/usr/bin/python2.7 /home/dmp/datax/bin/datax.py %s"%job_jsonpath)
