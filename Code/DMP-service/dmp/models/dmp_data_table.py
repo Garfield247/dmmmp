6a4870a02057d411001a870ab2ba7a1986603fec
CatMan <garfield_lv@163.com>
2020-06-08 09:40:48 +0800

    若干功能

--- Code/DMP-service/dmp/models/dmp_data_table.py
+++ Code/DMP-service/dmp/models/dmp_data_table.py
@@ -17,7 +17,8 @@ class DataTable(db.Model, DMPModel):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     dmp_data_table_name = db.Column(db.String(32), unique=True, nullable=False, comment='数据名称')
     db_table_name = db.Column(db.String(32), nullable=False, comment='数据库内数据表名称')
-    db_data_count = db.Column(db.Integer,default=data_count(),onupdate=data_count(),comment='数据表内的数据量')
+    # , default = data_count(), onupdate = data_count()
+    db_data_count = db.Column(db.Integer,comment='数据表内的数据量')
     description = db.Column(db.String(128), comment='数据说明')
     created_on = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now, comment='创建时间')
     changed_on = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now, comment='修改时间')
@@ -28,20 +29,20 @@ class DataTable(db.Model, DMPModel):
     # users = db.relationship('Users', backref='users_datatable')
     database = db.relationship('Database', backref='database_datatable')
     # case = db.relationship('Case', backref='case_datatable')
-    def data_count(self):
-        from dmp.engine import engines
-        Engine = engines.get(self.database.db_type)
-        if Engine:
-            conn = Engine(host=self.database.db_host,
-            port=self.database.db_port,
-            user = self.database.db_username,
-            passwd = self.database.db_passwd,
-            db = self.database.db_name,
-            )
-            count = conn.count(self.db_table_name)
-            return count
-        else:
-            return 0
+    # def data_count(self):
+    #     from dmp.engine import engines
+    #     Engine = engines.get(self.database.db_type)
+    #     if Engine:
+    #         conn = Engine(host=self.database.db_host,
+    #         port=self.database.db_port,
+    #         user = self.database.db_username,
+    #         passwd = self.database.db_passwd,
+    #         db = self.database.db_name,
+    #         )
+    #         count = conn.count(self.db_table_name)
+    #         return count
+    #     else:
+    #         return 0
 
     def delete(self):
         from dmp.models import DataTableColumn
