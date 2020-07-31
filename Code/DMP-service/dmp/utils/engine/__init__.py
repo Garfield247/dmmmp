d262eafa27727285f34cab66f384734c3df68c22
CatMan <garfield_lv@163.com>
2020-07-31 13:29:38 +0800

    [debug]migrate mysql2mysql

--- Code/DMP-service/dmp/utils/engine/__init__.py
+++ Code/DMP-service/dmp/utils/engine/__init__.py
@@ -32,9 +32,9 @@ def create_table_query_handler(table_name, fields, uniform_type, id_primary_key=
     id_pri = "id int  auto_increment primary key,"
     col = "{columns})"
     Delimiter = "row format delimited fields terminated by '{fieldDelimiter}'"
-    if id_primary_key and "id" in fields:
-        fields.remove("id")
-    columns = ",".join(["%s %s" % (col, uniform_type) for col in fields])
+    # if id_primary_key and "id" in fields:
+    #     fields.remove("id")
+    columns = ",".join(["%s %s" % (col, uniform_type) for col in fields if not (id_primary_key==True & col == "id")])
     p1 = id_pri if id_primary_key else ""
     p2 = Delimiter.format(fieldDelimiter=fieldDelimiter) if fieldDelimiter else ""
     p3 = ";" if semicolon else ""
