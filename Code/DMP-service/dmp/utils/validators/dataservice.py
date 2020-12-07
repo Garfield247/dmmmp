9227431784fe8551ec37972527743478d8740817
CatMan <garfield_lv@163.com>
2020-12-07 15:23:29 +0800

    fix(dataservice): params

--- Code/DMP-service/dmp/utils/validators/dataservice.py
+++ Code/DMP-service/dmp/utils/validators/dataservice.py
@@ -15,10 +15,10 @@ from validator import Validator, StringField, IntegerField, EnumField
 class Add_dataservice_validator(Validator):
     source_dmp_data_table_id = IntegerField(min_value=1, required=True)
     request_method = EnumField(choices=[1,2], required=True)
-    state = EnumField(choices=[0, 1], required=True)
+    state = EnumField(choices=[0, 1], required=False)
     data_service_name = StringField(max_length=64, required=True)
-    api_path = StringField(max_length=64, required=True)
-    query_sql = StringField(max_length=65535, required=True)
+    api_path = StringField(max_length=64, required=False)
+    query_sql = StringField(max_length=65535, required=False)
 
 class Update_dataservice_validator(Validator):
     source_dmp_data_table_id = IntegerField(min_value=1, required=False)
